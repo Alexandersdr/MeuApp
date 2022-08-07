53e48a15b13328c705d592797deefea1cc952647
Alexander <82269375+Alexandersdr@users.noreply.github.com>
2022-08-06 23:33:12 -0300

    0.0.4

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        //int idate = 25 ;
+        //int nome = alexander;
+        //var data = 06;
+       // var mes = ago;
+
+    //    const int IDADE_MINIMA;
+    //    const int IDADE_MINIMA = 25;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
